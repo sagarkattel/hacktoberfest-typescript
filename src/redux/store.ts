58dd6289fbededb23df0507741d010c5413c1e7c
sagarkattel <71958289+sagarkattel@users.noreply.github.com>
2023-10-01 15:41:26 +0530

    userRTK

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterSlice from './features/counterSlice';
 import todoSlice from './features/todoSlice';
+import { userApi } from './services/userApi';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
     reducer: {
       counter:counterSlice,
-      todo:todoSlice
+      todo:todoSlice,
+      [userApi.reducerPath]: userApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([userApi.middleware]),
   })
   
+  setupListeners(store.dispatch);
+
 
   // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
